b1e307dae014f592cc2342f7cb88bb5f6621ac59
rongyousu <su_maso@sina.com>
2018-11-22 13:14:30 +0800

    Update img2vector.py

--- code/src/util/img2vector.py
+++ code/src/util/img2vector.py
@@ -2,14 +2,14 @@
 # -*- coding=utf-8 -*-
 ###########################################################
 #
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
+# Copyright (c) 2017 , Inc. All Rights Reserved
 #
 ###########################################################
 """
 img2vector
 
 File: img2vector.py
-Author: surongyou(surongyou@100tal.com)
+Author: surongyou
 Date: 2017/05/08 16:28
 """
 
