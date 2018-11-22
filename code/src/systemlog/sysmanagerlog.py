ae063ef4bc402b11d844c95ae06b04fd8ba9be4f
rongyousu <su_maso@sina.com>
2018-11-22 13:19:46 +0800

    Update sysmanagerlog.py

--- code/src/systemlog/sysmanagerlog.py
+++ code/src/systemlog/sysmanagerlog.py
@@ -3,14 +3,14 @@
 # -*- coding=utf-8 -*-
 ###########################################################
 #
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
+# Copyright (c) 2017, Inc. All Rights Reserved
 #
 ###########################################################
 """
 manager log
 
 File: sysmanagerlog.py
-Author: surongyou(surongyou@100tal.com)
+Author: 
 Date: 2017/05/08 16:28
 """
 
