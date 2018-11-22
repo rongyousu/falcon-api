e2c35e694513a597b4e3258d4cf7bd2d6761f031
rongyousu <su_maso@sina.com>
2018-11-22 13:33:22 +0800

    Update mainresource.py

--- code/src/resources/mainresource.py
+++ code/src/resources/mainresource.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
-###########################################################
-#
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
-#
-###########################################################
-"""
 
-
-File: mainresource.py
-Author: surongyou(surongyou@100tal.com)
-Date: 2017/05/08 16:28
-"""
 
 import json
 import falcon
