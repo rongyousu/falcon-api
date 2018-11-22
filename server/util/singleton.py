782a8eb9ef5f95445a01079f0da702c35f0f92d1
rongyousu <su_maso@sina.com>
2018-11-22 13:02:19 +0800

    Update singleton.py

--- server/util/singleton.py
+++ server/util/singleton.py
@@ -1,13 +1,13 @@
 ###########################################################
 #
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
+# Copyright (c) 2017 , Inc. All Rights Reserved
 #
 ###########################################################
 """
 Singleton Definition
 
 File: singleton.py
-Author: wangliang(wangliang1@100tal.com)
+Author: 
 Date: 2017/05/08 19:05
 """
 
