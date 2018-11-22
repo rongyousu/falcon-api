68516255124e6f353d1cee44225c9ef599b61541
rongyousu <su_maso@sina.com>
2018-11-22 13:04:53 +0800

    Update test_trigger.py

--- server/trigger/test_trigger.py
+++ server/trigger/test_trigger.py
@@ -2,14 +2,14 @@
 # -*- coding=utf-8 -*-
 ###########################################################
 #
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
+# Copyright (c) 2017 , Inc. All Rights Reserved
 #
 ###########################################################
 """
 Test Trigger Defination
 
 File: test_trigger.py
-Author: wangliang(wangliang1@100tal.com)
+Author: 
 Date: 2017/05/05 19:55
 """
 
