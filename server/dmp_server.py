275c99f7ff7bfddbbcf4572ff8febc08097b9138
rongyousu <su_maso@sina.com>
2018-11-22 13:01:14 +0800

    Update dmp_server.py

--- server/dmp_server.py
+++ server/dmp_server.py
@@ -2,14 +2,14 @@
 # -*- coding=utf-8 -*-
 ###########################################################
 #
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
+# Copyright (c) 2017 , Inc. All Rights Reserved
 #
 ###########################################################
 """
 main schedule
 
 File: dmp_server.py
-Author: wangliang(wangliang1@100tal.com)
+Autho
 Date: 2017/05/05 16:28
 """
 
