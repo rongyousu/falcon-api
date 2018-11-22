9cf59320e2822f8ff24283d99045a4f50afb45f5
rongyousu <su_maso@sina.com>
2018-11-22 13:32:52 +0800

    Update accessdictmodel.py

--- code/src/schemas/accessdictmodel.py
+++ code/src/schemas/accessdictmodel.py
@@ -2,14 +2,14 @@
 # -*- coding=utf-8 -*-
 ###########################################################
 #
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
+# Copyright (c) 2017 , Inc. All Rights Reserved
 #
 ###########################################################
 """
 访问日志模型封装
 
 File: accessdictmodel.py
-Author: surongyou(surongyou@100tal.com)
+Author: 
 Date: 2017/05/10 20:28
 """
 
