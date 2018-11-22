fb08070ea0d847056bf7daf56a664245e3d2001c
rongyousu <su_maso@sina.com>
2018-11-22 13:14:53 +0800

    Update configmanager.py

--- code/src/util/configmanager.py
+++ code/src/util/configmanager.py
@@ -2,14 +2,14 @@
 # -*- coding=utf-8 -*-
 ###########################################################
 #
-# Copyright (c) 2017 xueersi.com, Inc. All Rights Reserved
+# Copyright (c) 2017 , Inc. All Rights Reserved
 #
 ###########################################################
 """
 user level config manager file
 
 File: configmanager.py
-Author: surongyou(surongyou@100tal.com)
+Author:
 Date: 2017/05/08 16:28
 """
 import ConfigParser
